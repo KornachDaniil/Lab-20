65a05945b44c7fbee58d3b83fbdbf7f69ce2cbeb
Kornach Daniil <danik.kornach@gmail.com>
2024-03-13 12:23:59 +0300

    Готова первая чать лабы

--- Lab№20/Lab№20.cpp
+++ Lab№20/Lab№20.cpp
@@ -1,6 +1,31 @@
 ﻿#include <iostream>
+#include <cmath>
+
+class Math {
+private:
+   double first;
+   double second;
+
+public:
+   Math(double &first, double &second) 
+   {// Конструктор
+      this->first = first;
+      this->second = second;
+   }
+
+   double hipotenuse()
+   {// Метод для нахождения гипотинузы
+      return sqrt(pow(first, 2) + pow(second, 2));
+   }
+};
 
 int main()
 {
-   
+   double first = rand() % 10;
+   std::cout << "First: " << first << std::endl;
+   double second = rand() % 10;
+   std::cout << "Second: " << second << std::endl;
+   Math a(first, second);
+   std::cout << "Hipotenuse: " << a.hipotenuse() << std::endl;
+   return 0;
 }
